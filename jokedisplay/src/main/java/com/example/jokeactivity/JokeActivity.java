f7fd1b7518875c5f4a077359f8f8c9d215f815e8
ayaRayan93 <aya.rayan1011@gmail.com>
2017-08-09 01:49:44 +0200

    Initial commit

---
+++ jokedisplay/src/main/java/com/example/jokeactivity/JokeActivity.java
@@ -0,0 +1,23 @@
+package com.example.jokeactivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class JokeActivity extends AppCompatActivity {
+
+   static public String JOKE_KEY="JOKE_KEY";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+        Intent intent = getIntent();
+        String joke = intent.getStringExtra(JOKE_KEY);
+
+        TextView jokeTextView = (TextView) findViewById(R.id.joke_textview);
+        if (joke != null && joke.length() != 0) {
+            jokeTextView.setText(joke);
+        }
+    }
+}
